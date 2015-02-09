31072dcfbf0e8ac5d8b21e9e76946116c4df359b
Eduard Kizmann <info@vanki.de>
2015-02-09 01:44:24 +0100

    Fixed chmod and email forwarding

--- init.sh
+++ init.sh
@@ -10,18 +10,28 @@ sh repository.sh > /dev/null
 
 echo "INSTALL VSFTPD..."
 sh vsftpd.sh > /dev/null
+service vsftpd reload
+service vsftpd restart
 
 echo "INSTALL POSTFIX..."
 sh postfix.sh > /dev/null
+service postfix reload
+service postfix restart
 
 echo "INSTALL NGINX..."
 sh nginx.sh > /dev/null
+service nginx reload
+service nginx restart
 
 echo "INSTALL PHP5-FPM..."
 sh php5-fpm.sh > /dev/null
+service php5-fpm reload
+service php5-fpm restart
 
 echo "INSTALL MYSQL..."
 sh mysql.sh > /dev/null
+service mysql reload
+service mysql restart
 
 echo "INSTALL GITLAB..."
 sh gitlab.sh > /dev/null
--- october.sh
+++ october.sh
@@ -2,3 +2,6 @@
 
 # Clone to public folder
 git clone https://github.com/octobercms/install.git /home/web/public
+
+# Setup write rights
+chmod 777 -R /home/web/public
